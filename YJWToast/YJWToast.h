22a97936b1dec0917c07742b943b049e7266d8c7
杨京为 <yangjingwei@yangjingweidemacbook-pro.local>
2018-08-29 15:19:16 +0800

    Initial Commit

---
+++ YJWToast/YJWToast.h
@@ -0,0 +1,19 @@
+//
+//  YJWToast.h
+//  YJWToast
+//
+//  Created by 杨京为 on 2018/8/29.
+//  Copyright © 2018年 YangJingwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YJWToast.
+FOUNDATION_EXPORT double YJWToastVersionNumber;
+
+//! Project version string for YJWToast.
+FOUNDATION_EXPORT const unsigned char YJWToastVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YJWToast/PublicHeader.h>
+
+
